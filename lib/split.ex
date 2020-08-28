ad5515042e53daf49d2b15add32fe46005782b6a
Linus Styrén <linus.styren@Linuss-MacBook-Pro.local>
2020-08-28 08:41:20 +0200

    Use strings instead of charlist

--- lib/split.ex
+++ lib/split.ex
@@ -15,4 +15,21 @@ defmodule Split do
   def hello do
     :world
   end
+  
+  def concat([], list), do: list
+  def concat([value], list), do: [value | list]
+  def concat([head | tail], list) do
+    [head | concat(tail, list)]
+  end
+
+  def split(str, del) do
+    [char | _] = String.to_charlist(del)
+    _split(String.to_charlist(str), char)
+  end
+
+  defp _split(list, char), do: _split(list, [], char)
+  defp _split(tail, [head | front], char) when char == head, do: concat([List.to_string(Enum.reverse(front))], _split(tail, char))
+  defp _split([], front, _), do: [List.to_string(Enum.reverse(front))]
+  defp _split([head | tail], front, char), do: _split(tail, [head | front], char)
+
 end
--- test/split_test.exs
+++ test/split_test.exs
@@ -6,4 +6,23 @@ defmodule SplitTest do
     assert Split.hello() == :world
   end
 
+  test "splits the string with space delimitter" do
+    assert Split.split("Hello World", " ") == ["Hello", "World"]
+  end
+
+  test "doesn't crash when no occurences of delimitter" do
+    assert Split.split("ABC", "D") == ["ABC"]
+  end
+
+  test "splits string with multiple delimitters" do
+    assert Split.split("I have many spaces", " ") == ["I", "have", "many", "spaces"]
+  end
+
+  test "splits string where first element is delimitter" do
+    assert Split.split(" ", " ") == ["",""]
+  end
+
+  test "splits empty string" do
+    assert Split.split("", " ") == [""]
+  end
 end
