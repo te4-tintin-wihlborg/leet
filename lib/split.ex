09c9a7ec27d15badfacee99c79a430a4041bcdb5
Linus Styrén <linus.styren@Linuss-MacBook-Pro.local>
2020-08-28 08:40:18 +0200

    Added code

---
+++ lib/split.ex
@@ -0,0 +1,18 @@
+defmodule Split do
+  @moduledoc """
+  Documentation for `Split`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Split.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/split_test.exs
@@ -0,0 +1,9 @@
+defmodule SplitTest do
+  use ExUnit.Case
+  doctest Split
+
+  test "greets the world" do
+    assert Split.hello() == :world
+  end
+
+end
