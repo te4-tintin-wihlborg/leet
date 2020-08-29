e411a8db9708efdca9d464ead15dc13245e03204
Linus Styrén <linus.styren@Linuss-MacBook-Pro.local>
2020-08-29 15:04:39 +0200

    handle edge case when no occurence of delimitter found

--- lib/split.ex
+++ lib/split.ex
@@ -17,6 +17,7 @@ defmodule Split do
   end
 
   def split(list, char), do: _split(list, [], char)
+  defp _split([], front, _), do: [Enum.reverse(front)]
   defp _split(tail, [head | front], char) when char == head, do: [Enum.reverse(front), tail]
   defp _split([head | tail], front, char), do: _split(tail, [head | front], char)
 
